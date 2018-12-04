736129a86737af2f47d48cc5160c063d555441fd
venupendiyala <vreddy.venu@gmail.com>
2018-12-04 15:42:57 -0500

    formatted code

--- api/helpers/publishJob.js
+++ api/helpers/publishJob.js
@@ -1,27 +1,27 @@
-const Queue = require('kue');
+const Queue = require('kue');
 
-const queue = Queue.createQueue({
-redis: process.env.REDIS_URL,
+const queue = Queue.createQueue({
+    redis: process.env.REDIS_URL,
 });
 
-const createJob = async (jobType) => {
-const job = queue.create(jobType, {
-    letter: 'a',
-    title: 'mytitle',
-}).removeOnComplete(true).save((error) => {
-if (error) {
-console.log(error);
-return;
-}
-job.on('complete', (result) => {
-console.log(`On Complete Hello Intense ${result}`);
-return result;
-});
-job.on('failed', () => {
-const failedError = new Error('failed');
-console.log(failedError);
-});
-});
+const createJob = async (jobType) => {
+    const job = queue.create(jobType, {
+        letter: 'a',
+        title: 'mytitle',
+    }).removeOnComplete(true).save((error) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        job.on('complete', (result) => {
+            console.log(`On Complete Hello Intense ${result}`);
+            return result;
+        });
+        job.on('failed', () => {
+            const failedError = new Error('failed');
+            console.log(failedError);
+        });
+    });
 };
 
-module.exports = { createJob }; 
+module.exports = { createJob }; 
--- verify.js
+++ verify.js
@@ -1,23 +1,23 @@
 
-const kue = require('kue');
+const kue = require('kue');
 
-try {
-    const queue = kue.createQueue({
-    redis: process.env.REDIS_URL,
-});
-queue.process('first', (job, done) => {
-    console.log('running the worker console');
+try {
+    const queue = kue.createQueue({
+        redis: process.env.REDIS_URL,
+    });
+    queue.process('first', (job, done) => {
+        console.log('running the worker console');
 
-switch (job.data.letter) {
-case 'a':
-console.log(job.data.letter);
-done(null, 'apple');
-break;
-default:
-done(null, 'unknown'); 
+        switch (job.data.letter) {
+            case 'a':
+                console.log(job.data.letter);
+                done(null, 'apple');
+                break;
+            default:
+                done(null, 'unknown');
+        }
+    });
+} catch (error) {
+    console.log(error);
 }
-});
-} catch (error) {
-console.log(error);
-} 
- 
+
