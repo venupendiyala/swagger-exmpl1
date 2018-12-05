dfd103da7db9d0688f2b85f49dced7cd8675b5c9
venupendiyala <vreddy.venu@gmail.com>
2018-12-05 10:28:13 -0500

    updating queue

--- workerQueueProcessing.js
+++ workerQueueProcessing.js
@@ -1,5 +1,44 @@
 const kue = require('kue');
-
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: {
+    personalizations: [
+      {
+        to: [
+          {
+            email: 'vpendiyala@dminc.com'
+          }
+        ],
+        subject: 'Sending with SendGrid is Fun'
+      }
+    ],
+    from: {
+      email: 'test@example.com'
+    },
+    content: [
+      {
+        type: 'text/plain',
+        value: 'and easy to do anywhere, even with Node.js'
+      }
+    ]
+  }
+});
+ 
+// With promise
+sg.API(request)
+  .then(function (response) {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  })
+  .catch(function (error) {
+    // error is an instance of SendGridError
+    // The full response is attached to error.response
+    console.log(error.response.statusCode);
+  });
+ 
 try {
     const queue = kue.createQueue({
         redis: process.env.REDIS_URL,
@@ -9,7 +48,6 @@ try {
         console.log(job.data.carName);
         //Logic to send Email to the user using SendGrid
         done(null, job.data.carName);
-        break;
     });
 } catch (error) {
     console.log(error);
